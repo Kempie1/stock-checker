6d23e7c4e38d771a9c3524a657c6bd4dc14e7e7b
Valentin <aplbep@outlook.com>
2021-02-21 16:54:35 +0100

    Setup: Added gitignore and app directory

---
+++ app/Stock Checker/Stock_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_CheckerApp.swift
+//  Stock Checker
+//
+//  Created by Valentin Silvera on 21.02.21.
+//
+
+import SwiftUI
+
+@main
+struct Stock_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
