3512a3743ef7ee5dd3b04eaeff2f31b2a546fc48
Max Hues <maximilian.hues@code.berlin>
2021-03-24 09:29:05 +0100

    working on mock for thrid party Api wp

--- api/TestCases/test_todos.py
+++ api/TestCases/test_todos.py
@@ -11,25 +11,12 @@ import requests
 # Standard library imports...
 from unittest.mock import Mock, patch
 
-# Third-party imports...
-from nose.tools import assert_is_not_none
-
-
 #Local Imports
 from services import get_todos
+from unittest.mock import Mock, patch
 
-
-@patch('services.requests.get')
-def test_getting_todos(mock_get):
-    # Configure the mock to return a response with an OK status code.
-    mock_get.return_value.ok = True
-
-    # Call the service, which will send a request to the server.
-    response = get_todos()
-
-    # If the request is sent successfully, then I expect a response to be returned.
-    assert_is_not_none(response)
-    
+# Third-party imports...
+from nose.tools import assert_is_none, assert_list_equal
 
 
 #Describition of the Test 
@@ -48,5 +35,72 @@ def test_request_response():
         # Confirm that the request-response cycle completed successfully.
         assert_true(response.ok)
 
+
+@patch('services.requests.get')
+def test_getting_todos(mock_get):
+    # Configure the mock to return a response with an OK status code.
+    #This is the fake responds 
+    mock_get.return_value.ok = True 
+
+    #This is the actual responds from the function get_todos
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    #if the function does not return a response then the assert will go of / the real api does not anwser
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
+    
+ #@patch() works: You provide it a path to the function you want to mock. 
+ #The function is found, patch() creates a Mock object, and the real function is temporarily replaced with the mock.
+ #When get_todos() is called by the test, the function uses the mock_get the same way it would use the real get() method. 
+ #That means that it calls mock_get like a function and expects it to return a response object.
+
+
+
+
+
+
+
+
+@patch('services.requests.get')
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    # Configure the mock to return a response with an OK status code. Also, the mock should have
+    # a `json()` method that returns a list of todos.
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_list_equal(response.json(), todos)
+
+
+@patch('services.requests.get')
+def test_getting_todos_when_response_is_not_ok(mock_get):
+    # Configure the mock to not return a response with an OK status code.
+    mock_get.return_value.ok = False
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the response contains an error, I should get no todos.
+    assert_is_none(response)
+
+
+
+
+
+
+
 test_request_response()
 test_getting_todos()
+test_getting_todos_when_response_is_ok()
+test_getting_todos_when_response_is_not_ok()
