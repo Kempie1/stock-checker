5c0e0c991dc4923a3ed3ca13025a687cd6a1afdf
Max Hues <maximilian.hues@code.berlin>
2021-04-08 18:31:30 +0200

    Added my view file to the project

---
+++ app/Stock Checker/Views/LearningView.swift
@@ -0,0 +1,20 @@
+//
+//  LearningView.swift
+//  Stock Checker
+//
+//  Created by Maximilian Hues on 08.04.21.
+//
+
+import SwiftUI
+
+struct LearningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LearningView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningView()
+    }
+}
