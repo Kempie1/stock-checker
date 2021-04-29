ac474f02e6ff526e98a80429891dc86128a395e4
Max Hues <maximilian.hues@code.berlin>
2021-04-29 15:22:05 +0200

    Deleted Tests that are not working

--- app/Stock CheckerUITests/Stock_CheckerUITests.swift
+++ app/Stock CheckerUITests/Stock_CheckerUITests.swift
@@ -19,13 +19,11 @@ class Stock_CheckerUITests: XCTestCase {
     }
     
     func testLearningViewQuizSucess(){
-        //Given The Tab Bar is clicked and Start Button
         app.tabBars["Tab Bar"].buttons["Learn"].tap()
         app.scrollViews.otherElements.containing(.staticText, identifier:"Section 1").children(matching: .button).matching(identifier: "Start").element(boundBy: 0).tap()
         
         let trueButton = self.app.buttons["TrueButton"]
         let falseButton = self.app.buttons["FalseButton"]
-//        let QuestionNumber = self.app.textFields["questionNumber"]    
         trueButton.tap()
         falseButton.tap()
         trueButton.tap()
@@ -38,15 +36,7 @@ class Stock_CheckerUITests: XCTestCase {
         falseButton.tap()
         trueButton.tap()
         falseButton.tap()
-        //What should the score be if the button is pressed if it is either false or true for eahc button
-        //Assert the percentage calculations
-        app.navigationBars.buttons.element(boundBy: 0).tap() //This is the back Button
-        //EVERY TEST SHOULD HAVE THAT:
-        //OPTIONAL(Given)
-        //When
-        //Then
-        //Assert
-        //ACT
+        app.navigationBars.buttons.element(boundBy: 0).tap()
     }
 
     func testLaunchPerformance() throws {
