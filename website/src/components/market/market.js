8fd8ab89ec2a3201bd96a1321ff721f5b00b35b4
Artem Miroshnichenko <Artteem8@gmail.com>
2021-05-02 15:22:34 -0500

    in progress

--- website/src/components/market/market.js
+++ website/src/components/market/market.js
@@ -1,12 +1,44 @@
-import React, {  useState  } from 'react';
+import React, {  useState, useEffect  } from 'react';
 import {  useAuth  } from "../../providers/AuthProvider"
 import { BrowserRouter as Router, Link, useHistory } from "react-router-dom"
-
+import axios from 'axios'
 
 const Market = (props) => {
-const [error, setError] = useState("")
+const [tickers, setTickers] = useState("")
 const { currentUser} = useAuth()
 
+useEffect(()=>{
+  axios.get(`https://stockcheckerdb.herokuapp.com/getavailable/`)
+  .then(res => {
+    setTickers(res.data.Stocks);
+  })
+  .catch(error => {
+    if (error.response) {
+      // Request made and server responded
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+  }
+  );
+}, [])
+//[Array(1), Array(1), Array(1)]
+function listStocks(){
+  //limit 10
+  //var count = 0
+  if (Array.isArray(tickers)){
+  tickers.map((value) => {
+    console.log(value[0])
+    return <p>{value}</p>}
+  )
+  }
+  }
 
 
 
@@ -15,7 +47,7 @@ return (
     <Link to="/stock/AAPL">
       Apple
     </Link>
-    
+    {listStocks()}
     </div>
     
   );
