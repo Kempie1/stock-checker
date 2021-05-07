29f1af5afd49ca34414a12eb48fabf60ee164813
Max Hues <maximilian.hues@code.berlin>
2021-05-07 17:24:31 +0200

    Added tests to test_thrid_party_api

--- api/ORM/services.py
+++ api/ORM/services.py
@@ -19,6 +19,7 @@ sys.path.append(config('APIFOLDER'))
 #Internal
 from declarative import Stock, Base
 from constants import ticker_symbol_for_testing
+
 def mock_api_request():
     response = requests.get("http://127.0.0.1:5000/json")
     return response
@@ -31,19 +32,18 @@ def real_api_request():
     response = requests.get(f"https://stockcheckerdb.herokuapp.com/getst/?ticker={ticker_symbol_for_testing}")
     return response
 
-def real_third_party_api_request():
-        ticker_symbol = ticker_symbol_for_testing
-        url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-statistics"
-        querystring = {"symbol": ticker_symbol,"region":"US"}
-        headers = {
-        'x-rapidapi-key': "92e0efc621msh368787aa782ea71p111f83jsn3acbf2f81995",
-        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
-        }
-        response = requests.get(url, headers=headers, params=querystring)
-        if response.ok:
-            return response
-        else:
-            return None
+def third_party_api_request(request):
+    url = f"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/{request}"   
+    querystring = {"symbol": ticker_symbol_for_testing,"region":"US"}
+    headers = {
+    'x-rapidapi-key': config("APIKEY"),
+    'x-rapidapi-host': config("APIHOST")
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.ok:
+        return response
+    else:
+        return None
 
 def real_sql_request():
     #This is creating an engine connected to the Database URL
@@ -57,5 +57,3 @@ def real_sql_request():
     #session = sessionmaker(bind=engine)
     #session.query(Stock).all()
     print("")
-
-mock_api_request
--- api/ORM/test_third_party_api.py
+++ api/ORM/test_third_party_api.py
@@ -11,50 +11,86 @@ from decouple import config
 import sys
 sys.path.append(config('ORMLogic'))
 sys.path.append(config('APIFOLDER'))
-
 #INTERNAL
-from services import real_third_party_api_request
+from services import third_party_api_request
 from constants import ticker_symbol_for_testing, ticker_symbol_for_request, ticker_symbol_table_full, ticker_symbol_table_empty, ticker_symbol_not_in_table
 #EXTERNAL
 from ORMLogic import api_call_to_json, ORM_services
-
 
 class test_Third_Party_Api_Integration(unittest.TestCase):
 
     #This Test should have probally be in the server itself
     def test_real_third_party_api_request_status(self):
         #Act
-        response = real_third_party_api_request()
-        #Assert
-        self.assertEqual(response.status_code, 200)
+        request = ["get-statistics", "get-financials", "get-chart"]
+        for i in request:
+            response = third_party_api_request(i)
+            #Assert
+            self.assertEqual(response.status_code, 200)
         
     def test_api_data(self):
         #Arrange
         constant_input = ticker_symbol_for_testing
         #Act
-        response = real_third_party_api_request()
+        response = third_party_api_request("get-statistics")
         #Assert
         json_response = response.json()
         self.assertEqual(json_response['symbol'], constant_input)
 
-    def test_third_party_api_dict_financials_keys(self):
-        list_financial_keys = ['balanceSheetHistory', 'balanceSheetHistoryQuarterly', 'cashflowStatementHistory', 'cashflowStatementHistoryQuarterly', 'earnings', 'errorList', 'financialsTemplate', 'incomeStatementHistory', 'incomeStatementHistoryQuarterly', 'loading', 'meta', 'pageViews', 'price', 'quoteType', 'summaryDetail', 'symbol', 'timeSeries']
-        response = real_third_party_api_request()
-        response_body = response.json()
-        response_keys = []
-        for key in response_body:
-            response_keys.append(key)
-        self.assertEqual(list_financial_keys,response_keys)
+    def test_api_main_keys(self):
+        request = ["get-statistics", "get-financials", "get-chart"]
+        dict_statistics = {}
+        dict_financials = {}
+        dict_chart = {}
+        for i in request:
+            response = third_party_api_request(i)
+            response_json = response.json()
+            if i == "get-statistics":
+                dict_statistics["get-statistics"] = response_json
+            if i == "get-financials":
+                dict_financials["get-financials"] = response_json
+            if i == "get-chart":
+                dict_chart["get-chart"] = response_json
+        merge_all_dicts = {**dict_statistics, **dict_financials, **dict_chart}
+        expected_keys = "dict_keys(['get-statistics', 'get-financials', 'get-chart'])"
+        response_keys = str(merge_all_dicts.keys())
+        self.assertEqual(expected_keys, response_keys)
 
-    def test_third_party_api_dict_statistics_keys(self):
-        list_statistics_keys = ['calendarEvents', 'defaultKeyStatistics', 'financialData', 'financialsTemplate', 'mktmData', 'pageViews', 'price', 'quoteData', 'quoteType', 'summaryDetail', 'symbol']
-        #print(response_body['get-statistics']['calendarEvents'].keys())
-        response = real_third_party_api_request()
-        response_body = response.json()
-        response_keys = []
-        for key in response_body:
-            response_keys.append(key)
-        self.assertEqual(list_statistics_keys,response_keys)
+    def test_api_statistics_nested_keys(self):
+        response = third_party_api_request("get-statistics")
+        response_json = response.json()
+        dict_statistics = {}
+        dict_statistics["get-statistics"] = response_json
+        list_of_keys_main_statistics = []
+        for key, value in dict_statistics.items():
+            for keys, values in dict_statistics[key].items():
+                list_of_keys_main_statistics.append(keys)
+        expected_keys = ['defaultKeyStatistics', 'financialsTemplate', 'price', 'financialData', 'quoteType', 'calendarEvents', 'summaryDetail', 'symbol', 'pageViews', 'quoteData', 'mktmData']
+        self.assertEqual(list_of_keys_main_statistics, expected_keys)
+
+    def test_api_financials_nested_keys(self):
+        response = third_party_api_request("get-financials")
+        response_json = response.json()
+        dict_financials = {}
+        dict_financials["get-financials"] = response_json
+        list_of_keys_main_financials = []
+        for key, value in dict_financials.items():
+            for keys, values in dict_financials[key].items():
+                list_of_keys_main_financials.append(keys)
+        expected_keys = ['financialsTemplate', 'cashflowStatementHistory', 'balanceSheetHistoryQuarterly', 'earnings', 'price', 'incomeStatementHistoryQuarterly', 'incomeStatementHistory', 'balanceSheetHistory', 'cashflowStatementHistoryQuarterly', 'quoteType', 'summaryDetail', 'symbol', 'pageViews', 'timeSeries', 'meta', 'loading', 'errorList']
+        self.assertEqual(list_of_keys_main_financials, expected_keys)
+
+    def test_api_chart_nested_keys(self):
+        response = third_party_api_request("get-chart")
+        response_json = response.json()
+        dict_chart = {}
+        dict_chart["get-chart"] = response_json
+        list_of_keys_main_chart = []
+        for key, value in dict_chart.items():
+            for keys, values in dict_chart[key].items():
+                list_of_keys_main_chart.append(keys)
+        expected_keys = ['chart']
+        self.assertEqual(list_of_keys_main_chart, expected_keys)
 
     def test_json_data(self):
         #Arrange
@@ -66,11 +102,10 @@ class test_Third_Party_Api_Integration(unittest.TestCase):
         #Assert    
         self.assertEqual(ticker_symbol_json, constant_input)
 
-#if __name__ == '__main__':
- #   unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
-Api_Integration_test = test_Third_Party_Api_Integration()
-Api_Integration_test.test_third_party_api_dict_financials_keys()
-#Api_Integration_test.test_api_request_status()
+#Api_Integration_test = test_Third_Party_Api_Integration()
+#Api_Integration_test.test_api_data()
 #Api_Integration_test.test_api_data()
 #Api_Integration_test.test_json_data()
