c29d2716ab1484a2735cc08e420cab8b2213a90e
Artem Miroshnichenko <Artteem8@gmail.com>
2021-05-25 06:53:40 -0500

    minor changes

--- website/src/components/stock/stock.test.js
+++
@@ -1,6 +0,0 @@
-import { timeConverter } from "./stock"
-
-test("timeConverter", () =>{
-    expect(timeConverter(1672444800)).toBe("30 Dec 2022")
-    expect(timeConverter(1617148800)).toBe("30 Mar 2021")
-})
